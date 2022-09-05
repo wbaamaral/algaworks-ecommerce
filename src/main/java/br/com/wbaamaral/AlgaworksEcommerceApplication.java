80c9f36547c6e5bff08ef58cf50238b50d318ee1
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2022-09-04 23:43:02 -0400

    Especialista JPA - inicio do estudo

---
+++ src/main/java/br/com/wbaamaral/AlgaworksEcommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.wbaamaral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaworksEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaworksEcommerceApplication.class, args);
+	}
+
+}
